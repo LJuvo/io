a7734182c66e688a376734d9c35dc68d028588e1
JuvoS <www.704921698@qq.com>
2019-07-11 00:14:04 +0800

    some

---
+++ src/views/house/rear/comps/BasicRear.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="basic">
+    <div class="basic-content">内容</div>
+    <div class="basic-footer">footer</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.basic {
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  &-footer {
+    height: 50px;
+    line-height: 50px;
+    font-size: 14px;
+    text-align: center;
+    width: 100%;
+  }
+  &-content {
+    flex: 1;
+  }
+}
+</style>

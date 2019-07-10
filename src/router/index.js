e99bcd2b8c2839b678e5efbe2e4e72acb028e618
LJuvo <www.704921698@qq.com>
2019-07-10 20:23:12 +0800

    svg

--- src/router/index.js
+++ src/router/index.js
@@ -11,47 +11,55 @@ Vue.use(Router);
 // const Home = resolve => require(["../views/Home.vue"], resolve);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      component: Home
-    },
-    {
-      path: "/gem",
-      component: Gem
-    },
-    {
-      path: "/house",
-      component: House
-    },
-    {
-      path: "/about",
-      name: "关于",
-      component: () => import("../views/About.vue")
-    },
-    {
-      path: "/test",
-      name: "组件测试",
-      component: () => import("../views/Test/")
-    },
-    {
-      path: "/404",
-      name: "未知页",
-      component: () => import("../views/404/")
-    }
-    // {
-    //   path: "/",
-    //   component: Root,
-    //   redirect: "/home",
-    //   children: [
-    //     {
-    //       path: "home",
-    //       component: Home,
-    //       name: "首页"
-    //     }
-    //   ]
-    // }
-  ]
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+            path: "/",
+            component: Home
+        },
+        {
+            path: "/gem",
+            component: Gem
+        },
+        {
+            path: "/house",
+            component: House
+        },
+        {
+            path: "/about",
+            name: "关于",
+            component: () =>
+                import ("../views/About.vue")
+        },
+        {
+            path: "/test",
+            name: "组件测试",
+            component: () =>
+                import ("../views/Test/")
+        },
+        {
+            path: "/svg",
+            name: "SVG组件",
+            component: () =>
+                import ("../views/svg/")
+        },
+        {
+            path: "/404",
+            name: "未知页",
+            component: () =>
+                import ("../views/404/")
+        }
+        // {
+        //   path: "/",
+        //   component: Root,
+        //   redirect: "/home",
+        //   children: [
+        //     {
+        //       path: "home",
+        //       component: Home,
+        //       name: "首页"
+        //     }
+        //   ]
+        // }
+    ]
 });
---
+++ src/views/svg/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="basic-pane">
+    <div class="basic-pane-item" v-for="(item,key) in imgArr" :key="key">
+      <img :src="item.url" />
+      <span>{{item.label}}</span>
+    </div>
+  </div>
+</template>
+<script>
+import { svgArr } from "../../api/svg";
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    imgArr() {
+      return svgArr;
+    }
+  },
+  mounted() {},
+  methods: {
+    init() {},
+    start() {},
+    restart() {},
+    load() {},
+    reload() {},
+    destory() {}
+  },
+  beforeDestroy() {}
+};
+</script>
+<style lang='less' scoped>
+.basic-pane {
+  width: 100%;
+  height: 100%;
+  background: #333;
+  overflow: hidden;
+  overflow-y: auto;
+  display: grid;
+  grid-template-columns: 20% 20% 20% 20% 20%;
+  &-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    text-align: center;
+    padding: 10px;
+    img {
+      width: 100%;
+      height: 100px;
+    }
+    span {
+      margin: 10px auto;
+      font-size: 16px;
+      color: #eeeeee;
+    }
+  }
+}
+</style>

6223852cb58c52ef274094bd95130107c2d04537
JuvoS <www.704921698@qq.com>
2019-11-17 23:12:59 +0800

    add nine

--- src/router/index.js
+++ src/router/index.js
@@ -11,79 +11,78 @@ Vue.use(Router);
 // const Home = resolve => require(["../views/Home.vue"], resolve);
 
 export default new Router({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes: [{
-            path: "/",
-            component: Home
-        },
-        {
-            path: "/gem",
-            component: Gem
-        },
-        {
-            path: "/house",
-            component: House
-        },
-        {
-            path: "/about",
-            name: "关于",
-            component: () =>
-                import ("../views/About.vue")
-        },
-        {
-            path: "/test",
-            name: "组件测试",
-            component: () =>
-                import ("../views/Test/")
-        },
-        {
-            path: "/menu",
-            name: "菜单选择测试",
-            component: () =>
-                import ("../views/Test/menuCheck")
-        },
-        {
-            path: "/graph",
-            name: "MxGraph测试",
-            component: () =>
-                import ("../views/Test/graphP")
-        },
-        {
-            path: "/study",
-            name: "MxGraph学习",
-            component: () =>
-                import ("../views/study/graphP.vue")
-        },
-        {
-            path: "/editor",
-            name: "编辑器",
-            component: () =>
-                import ("../views/editor")
-        },
-        {
-            path: "/svg",
-            name: "SVG组件",
-            component: () =>
-                import ("../views/svg/")
-        },
-        {
-            path: "/404",
-            name: "未知页",
-            component: () =>
-                import ("../views/404/")
-        }
-        // {
-        //   path: "/",
-        //   component: Root,
-        //   redirect: "/home",
-        //   children: [
-        //     {
-        //       path: "home",
-        //       component: Home,
-        //       name: "首页"
-        //     }
-        //   ]
-        // }
-    ]
+  // mode: "history",
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/gem",
+      component: Gem
+    },
+    {
+      path: "/house",
+      component: House
+    },
+    {
+      path: "/nine",
+      name: "Nine",
+      component: () => import("../views/Nine/")
+    },
+    {
+      path: "/about",
+      name: "关于",
+      component: () => import("../views/About.vue")
+    },
+    {
+      path: "/test",
+      name: "组件测试",
+      component: () => import("../views/Test/")
+    },
+    {
+      path: "/menu",
+      name: "菜单选择测试",
+      component: () => import("../views/Test/menuCheck")
+    },
+    {
+      path: "/graph",
+      name: "MxGraph测试",
+      component: () => import("../views/Test/graphP")
+    },
+    {
+      path: "/study",
+      name: "MxGraph学习",
+      component: () => import("../views/study/graphP.vue")
+    },
+    {
+      path: "/editor",
+      name: "编辑器",
+      component: () => import("../views/editor")
+    },
+    {
+      path: "/svg",
+      name: "SVG组件",
+      component: () => import("../views/svg/")
+    },
+    {
+      path: "/404",
+      name: "未知页",
+      component: () => import("../views/404/")
+    }
+    // {
+    //   path: "/",
+    //   component: Root,
+    //   redirect: "/home",
+    //   children: [
+    //     {
+    //       path: "home",
+    //       component: Home,
+    //       name: "首页"
+    //     }
+    //   ]
+    // }
+  ]
 });
---
+++ src/views/Nine/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="sea">
+    <header class="sea-left">
+      <div class="sea-left-menu">Home</div>
+      <div class="sea-left-footer">Juvos.</div>
+    </header>
+    <section class="sea-content"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onRouter(val) {
+      this.$router.push(val);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.sea {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  &-left {
+    width: 200px;
+    background: #fff;
+    box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    &-menu {
+      flex: 1;
+    }
+    &-footer {
+      height: 120px;
+      width: 200px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  &-content {
+    flex: 1;
+    min-width: 1000px;
+    background: rgba(199, 203, 208, 1);
+  }
+}
+</style>
